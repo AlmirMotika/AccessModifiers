ec2cf3ab29fe9dbd5421ec4c0067c09fe09b05b2
Almir Motika <almir.motika@edu.fit.ba>
2022-04-28 23:23:33 +0200

    Add project files.

---
+++ Assembly1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembly1
+{
+    public class Assembly1BaseClass
+    {
+        private string privateVariable = "private";
+        protected string protectedVariable = "protected";
+        internal string internalVariable = "internal";
+        protected internal string protectedInternalVariable = "protected internal";
+        public string publicVariable = "public";
+    
+
+        public void TestAccess()
+        {
+            //Accessible
+            Console.WriteLine(privateVariable);
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(internalVariable);
+            Console.WriteLine(protectedInternalVariable);
+            Console.WriteLine(publicVariable);
+
+        }
+
+
+    }
+    public class Assembly1DerivedClass : Assembly1BaseClass
+    {
+        public void TestAccessInDerivedClass()
+        {
+            //Not Accessible
+            //Console.WriteLine(privateVariable);
+            
+            //Accessible
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(internalVariable);
+            Console.WriteLine(protectedInternalVariable);
+            Console.WriteLine(publicVariable);
+        }
+    }
+    public class Assembly1OtherClass
+    {
+        public void TestAccess()
+        {
+            Assembly1BaseClass objBaseClass = new Assembly1BaseClass();
+            //Not accessible
+            //Console.WriteLine(objBaseClass.privateVariable);
+            //Console.WriteLine(objBaseClass.protectedVariable);
+            Console.WriteLine(objBaseClass.internalVariable);
+            Console.WriteLine(objBaseClass.protectedInternalVariable);
+            Console.WriteLine(objBaseClass.publicVariable);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assembly1BaseClass objBase = new Assembly1BaseClass();
+            //objBase.TestAccess();
+            //Assembly1DerivedClass derivedClass = new Assembly1DerivedClass();
+            //derivedClass.TestAccessInDerivedClass();
+            Assembly1OtherClass otherClass = new Assembly1OtherClass();
+            otherClass.TestAccess();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assembly2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Assembly1;
+
+namespace Assembly2
+{
+    public class Assembly2DerivedClass : Assembly1BaseClass
+    {
+        public void TestAccessInDerivedClass()
+        {
+            //Not accesible
+            //Console.WriteLine(privateVariable);
+            // Console.WriteLine(internalVariable);
+
+            //Accessible
+            Console.WriteLine(protectedVariable);
+            Console.WriteLine(protectedInternalVariable);
+            Console.WriteLine(publicVariable);
+        }
+    }
+    public class Assembly2OtherClass
+    {
+        public void TestAccess()
+        {
+            Assembly1BaseClass objBaseClass = new Assembly1BaseClass();
+            //Not accessible
+            //Console.WriteLine(objBaseClass.privateVariable);
+            //Console.WriteLine(objBaseClass.protectedVariable);
+            //Console.WriteLine(objBaseClass.internalVariable);
+            //Console.WriteLine(objBaseClass.protectedInternalVariable);
+
+            //Accessible
+            Console.WriteLine(objBaseClass.publicVariable);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assembly2DerivedClass obj = new Assembly2DerivedClass();
+            //obj.TestAccessInDerivedClass();
+
+            //obj.TestAccess();
+
+            Assembly2OtherClass objOtherClass = new Assembly2OtherClass();
+
+            objOtherClass.TestAccess();
+            Console.ReadKey();
+        }
+    }
+}
